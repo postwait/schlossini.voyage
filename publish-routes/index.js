0239fa69a1c44a720fdf284bd4d8ccd7c18926a2
Theo Schlossnagle <jesus@omniti.com>
2015-06-10 20:35:12 -0400

    basic publishing

--- publish-routes/index.js
+++ publish-routes/index.js
@@ -1,7 +1,11 @@
 'use strict';
 
+var Voyage = require('../lib/voyage');
 var express = require('express');
+var showdown = require('../public/js/showdown')
+var converter = new showdown.Converter();
 var router = express.Router();
+var require_voyage = Voyage.express_voyage
 
 router.param(function(name, fn){
   if (fn instanceof RegExp) {
@@ -21,23 +25,62 @@ var tp = require('../travel-posts');
 var travel_posts = new tp('./posts/.');
 
 router.param('date', /^(\d{4}-\d{2}-\d{2})?$/);
-router.param('person', /^([a-z]+)$/);
+router.param('person', /^([0-9]+)$/);
 /* GET home page. */
-router.get('/', function(req, res) {
-  var date = travel_posts.getDate(req,res);
-  res.render('index', { title: 'Schlossini Voyage',
-                        dateparam: undefined,
-                        date: date,
-                        TP: travel_posts,
-                        posts: travel_posts.frontage(date,6) });
-});
-router.get('/itinerary', function(req, res) {
-  var date = travel_posts.getDate(req,res);
-  res.render('itinerary', { title: 'Schlossini Voyage Itinerary',
-                        dateparam: undefined,
-                        date: date,
-                        TP: travel_posts });
-});
+router.get('/', require_voyage(function(req, res) {
+  Voyage.trips(req.tresbon.voyage, function(err, trips) {
+    trips = trips.filter(function(t) { return t.active });
+    if(trips.length == 1) {
+      return res.redirect('/t/' + trips[0].url_snippet)
+    }
+    res.render('index', { voyage: req.tresbon.voyage,
+                        dataparam: undefined,
+                        date: req.tresbon.date,
+                        trips: trips,
+                        travelers: null,
+                        waypoints: null,
+                        trip: null,
+                        })
+  });
+}))
+router.get('/t/:trip', require_voyage(function(req, res) {
+  Voyage.tripBySlug(req.params.trip, req.tresbon.voyage, function(err, trip) {
+    Voyage.tripPosts(req.tresbon.voyage, trip.tripid,
+                     { date: req.tresbon.date,
+                       include_json: true,
+                       limit: 6 },
+      function(err, posts) {
+        posts.forEach(function(p) { p.data.html = converter.makeHtml(p.data.body) })
+        res.render('trip', { voyage: req.tresbon.voyage,
+                             trip: trip,
+                             dateparam: undefined,
+                             date: req.tresbon.date,
+                             travelers: null,
+                             waypoints: null,
+                             posts: posts });
+      });
+  });
+}));
+router.get('/t/:trip/itinerary', require_voyage(function(req, res) {
+  // Itinerary is a monster, all waypoints, all posts, all travelers
+  Voyage.travelers(req.tresbon.voyage, {date: req.tresbon.date}, function(err, travelers) {
+    Voyage.tripBySlug(req.params.trip, req.tresbon.voyage, function(err, trip) {
+      Voyage.tripPosts(req.tresbon.voyage, trip.tripid, {date: req.tresbon.date}, function(err, posts) {
+        Voyage.tripWaypoints(req.tresbon.voyage, trip.tripid, {date: null}, function(err, points) {
+          posts = posts.filter(function(p) { return p.published });
+          points = points.filter(function(p) { return p.visibility > 0; })
+          res.render('itinerary', { voyage: req.tresbon.voyage,
+                                    trip: trip,
+                                    dateparam: undefined,
+                                    date: req.tresbon.date,
+                                    travelers: travelers,
+                                    posts: posts,
+                                    waypoints: points, console: console });
+        });
+      })
+    })
+  })
+}));
 router.get('/:date', function(req, res) {
   var dateparam = req.params.date[1];
   var date = new Date(dateparam);
--- publish-routes/traveler.js
+++ publish-routes/traveler.js
@@ -1,7 +1,11 @@
 'use strict';
 
+var Voyage = require('../lib/voyage');
 var express = require('express');
+var showdown = require('../public/js/showdown')
+var converter = new showdown.Converter();
 var router = express.Router();
+var require_voyage = Voyage.express_voyage;
 
 var default_perpage = 3;
 
@@ -22,34 +26,59 @@ router.param(function(name, fn){
 var tp = require('../travel-posts');
 var travel_posts = new tp('./posts/.');
 
-router.param('person', /^([a-z]+)$/);
-router.param('title', /^([^\/]+)$/);
+router.param('person', /^([0-9]+)$/);
 /* GET home page. */
-router.get('/:person', function(req, res) {
+router.get('/:person/on/:trip', require_voyage(function(req, res, next) {
   var date = travel_posts.getDate(req,res);
-  var person = req.params.person[1];
   var nelem = parseInt(req.query.perpage || default_perpage);
   var pageno = parseInt(req.query.page || 1);
-  res.render('person', { title: 'Schlossini Voyage',
-                        person: person,
-                        pageno: pageno,
-                        nelem: nelem,
-                        date: date,
-                        dateparam: undefined,
-                        TP: travel_posts,
-                        posts: travel_posts.newest(date, person) });
-});
+  Voyage.travelers(req.tresbon.voyage, {date: req.tresbon.date}, function(err, travelers) {
+    Voyage.tripBySlug(req.params.trip, req.tresbon.voyage, function(err, trip) {
+      Voyage.tripPosts(req.tresbon.voyage, trip.tripid,
+                       { date: req.tresbon.date,
+                         include_json: true,
+                         limit: nelem,
+                         offset: (pageno - 1) * nelem },
+        function(err, posts) {
+          var me = travelers.filter(function(t) { return t.userid == req.params.person[1]; })
+          if(me.length != 1) return next();
+          posts.forEach(function(post) {post.data.html = converter.makeHtml(post.data.body);});
+          res.render('person', { voyage: req.tresbon.voyage,
+                                 person: me[0],
+                                 trip: trip,
+                                 dateparam: undefined,
+                                 date: req.tresbon.date,
+                                 travelers: null,
+                                 waypoints: null,
+                                 pageno: pageno,
+                                 nelem: nelem,
+                                 posts: posts });
+        });
+    });
+  });
+}));
 
-router.get('/:person/writes/:title', function(req, res) {
-  var person = req.params.person[1];
-  var title = req.params.title[1];
-  var post = travel_posts.post(person, title);
-  res.render('single', { title: post.title || 'Schlossini Voyage',
+router.get('/:person/on/:trip/writes/:title', require_voyage(function(req, res, next) {
+  Voyage.tripBySlug(req.params.trip, req.tresbon.voyage, function(err, trip) {
+    Voyage.tripPosts(req.tresbon.voyage, trip.tripid,
+                     { date: req.tresbon.date,
+                       include_json: true,
+                       url_snippet: req.params.title },
+      function(err, posts) {
+        if(!posts || posts.length != 1) return next();
+        var post = posts[0]
+        post.data.html = converter.makeHtml(post.data.body);
+        var person = req.params.person[1];
+        res.render('single', { title: post.title || req.tresbon.voyage.title,
                         person: person,
-                        date: post.date,
+                        date: post.whece,
                         dateparam: undefined,
+                        waypoints: null,
+                        travelers: null,
                         TP: travel_posts,
                         post: post });
-});
+      });
+    });
+}));
 
 module.exports = router;
